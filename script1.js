6b907085b90efbea3a807c5418ea3666f91596f6
automatedhumanity <134726046+automatedhumanity@users.noreply.github.com>
2023-05-26 12:48:43 -0400

    newjs

---
+++ script1.js
@@ -0,0 +1,5 @@
+const button = document.querySelector ('.button');
+button.addEventListener('click', function() {
+const heading = document.querySelector ('.p1');
+heading.textContent = 'this is a new heading'
+})
